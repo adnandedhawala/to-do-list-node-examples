91820708c7c94f424c7ca55243cc490f15826c01
adnandedhawala <adnan.d@somaiya.edu>
2022-12-20 22:27:16 +0530

    added code for all routes

--- examples/node_express/src/validators/index.ts
+++ examples/node_express/src/validators/index.ts
@@ -11,14 +11,12 @@ export const createTodoSchema = yup.object().shape({
 });
 
 export const editTodoSchema = yup.object().shape({
-  _id: yup.string().required("id is required"),
   title: yup.string().min(3, "title too short"),
   description: yup.string(),
   status: yup.string().oneOf(Object.values(TODO_STATUS), "invalid status"),
 });
 
 export const statusChange = yup.object().shape({
-  _id: yup.string().required("id is required"),
   status: yup
     .string()
     .required("status is required")
